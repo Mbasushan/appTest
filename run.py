85a1c13d180c14561b870a5a3bf162d8abd4660f
sushan <756715212@qq.com>
2019-05-13 16:04:26 +0800

    测报报告生成

--- run.py
+++ run.py
@@ -4,18 +4,24 @@
 import os
 import time
 import unittest
+import sys
+
+import HTMLTestRunner_py3
 from HTMLTestRunner import HTMLTestRunner
 
 test_dir = './testcase'
 discover = unittest.defaultTestLoader.discover(start_dir='./testcase', pattern="*.py")
 
 if __name__ == "__main__":
+
     report_dir = './test_report'
     os.makedirs(report_dir, exist_ok=True)
     now = time.strftime("%Y-%m-%d %H-%M-%S")
     report_name = '{0}/{1}.html'.format(report_dir, now)
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
     with open(report_name, 'wb')as f:
-        runner = HTMLTestRunner(stream=f, title="测试报告", description="本测试报告内容为APP搜索测试")
+        #runner = HTMLTestRunner(stream=f, title="测试报告", description="本测试报告内容为APP搜索测试")
+        runner = HTMLTestRunner_py3.HTMLTestRunner(stream=f,verbosity=2,title='android自动化测试报告',description='执行人：sxs')
         runner.run(discover)
 
